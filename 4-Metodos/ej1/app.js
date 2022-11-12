0275e6dfe15641e7ea974d9c0a928c4789d743f0
Vir Flores <vfcon.graphics@gmail.com>
2022-11-12 23:05:40 +0100

    six commit

--- 4-Metodos/ej1/app.js
+++ 4-Metodos/ej1/app.js
@@ -1,29 +1,55 @@
 const express = require('express')
 const app = express()
 
+//Imports
+let personas = require('./personas')
 
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-//Imports
-let personas = require ('./personas')
 
-app.get('/personas', (req, res)=>{
-    
-    res.send({personas})
+
+app.get('/personas', (req, res) => {
+
+    res.send(personas)
 
 })
 
-app.post('/personas', (req,res)=>{
-    let nombre = req.body.personas[0].nombre
-    res.send('Hola ' + nombre)
+app.post('/agregar', (req, res) => {
+    let persona = req.body
+    personas.push(persona)
+    res.send({mensaje:"Persona añadida", status: 200})
+})
+
+app.put('/modificar', (req, res)=>{
+    let i = personas.findIndex(persona => persona.nombre === req.body.nombre)
+    if (i < 0){
+    res.send({mensaje: "No se ha encontrado a " + req.body.nombre})
+}else{
+    personas[i].nombre = req.body.nombre
+    personas[i].apellido = req.body.apellido
+    personas[i].edad = req.body.edad
+    res.send({mensaje:`Se ha modificado a ${req.body.nombre} en la base de datos`, status: 200})
+}
+
+})
+
+app.delete('/borrar', (req, res)=>{
+    let i = personas.findIndex(persona => persona.nombre === req.body.nombre)
+    if (i < 0){
+    res.send({mensaje: "No se ha encontrado a " + req.body.nombre})
+}else{
+    personas.splice(i,1)
+    res.send({mensaje:`Se ha borrado a ${req.body.nombre} de la base de datos`, status: 200})
+}
+
 })
 
 
 let port = process.env.PORT || 3000
-app.listen(port, err =>{
+app.listen(port, err => {
     err
-    ? console.error('He fallado')
-    : console.log('Estoy funcionando en http://localhost:' + port)
+        ? console.error('He fallado')
+        : console.log('Estoy funcionando en http://localhost:' + port)
 })
--- 4-Metodos/ej1/personas.js
+++ 4-Metodos/ej1/personas.js
@@ -1,16 +1,24 @@
-  let personas = [
-    {nombre:"Cristina",
-    apellido:"Gonzalez",
-edad:38,},
-{nombre:"Luis",
-    apellido:"Flores",
-edad:29,},
-{nombre:"Sandra",
-    apellido:"Isardo",
-edad:37,},
-{nombre:"Rafael",
-    apellido:"Franco",
-edad:39,},
-  ] 
+let personas = [
+    {
+        nombre: "Cristina",
+        apellido: "Gonzalez",
+        edad: 38,
+    },
+    {
+        nombre: "Luis",
+        apellido: "Flores",
+        edad: 29,
+    },
+    {
+        nombre: "Sandra",
+        apellido: "Isardo",
+        edad: 37,
+    },
+    {
+        nombre: "Rafael",
+        apellido: "Franco",
+        edad: 39,
+    },
+]
 
-  module.exports = personas
+module.exports = personas
